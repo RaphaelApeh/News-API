745ffe0e5cc25d7d8b452bb77a13897d953024c6
raphael apeh <raphaelapeh37@gmail.com>
2025-02-14 11:31:12 +0100

    add jwt authentication

--- posts/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import generics
-
-from ..models import Post
-from ..serializers import PostSerializer
-
-
-class PostListView(generics.ListAPIView):
-    queryset = Post.objects.select_related("user")
-    serializer_class = PostSerializer
