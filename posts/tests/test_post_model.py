8765346f4e0f66605474253dc1811439e69f11d2
raphael apeh <raphaelapeh37@gmail.com>
2025-02-17 22:22:02 +0100

    added test for Post model

---
+++ posts/tests/test_post_model.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.utils.lorem_ipsum import WORDS
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+from posts.models import Post
+
+User = get_user_model()
+
+class PostTestCase(TestCase):
+
+
+    def setUp(self)-> None:
+        user = User.objects.create_user("testuser", "testpassword")
+        Post.objects.create(user=user, title="A good article post", content=" ".join(WORDS))
+
+    def test_post_create(self):
+
+        user = User.objects.create_user("admin", "testpassword")
+        Post.objects.create(user=user, title="A terrible title", content=" ".join(WORDS))
+
+    def test_invalid_post_create(self):
+
+        user = User.objects.create_user("admin", "testpassword")
+        post = Post.objects.create(user=user, content=" ".join(WORDS)) # No title
+
+        with self.assertRaises(ValidationError):
+            post.full_clean()
+    
+    def test_reverse_comment_create(self):
+        
+        post = Post.objects.first()
+        post.posts.create(user=post.user, content="A good article")
+        
+        self.assertTrue(post.posts.exists())
