36c94367ce48c0107a23499e32358ac9d1a67d62
raphael apeh <raphaelapeh37@gmail.com>
2025-03-19 17:04:33 +0100

    fixed post slug

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.conf import settings
+from django.utils.crypto import get_random_string
 from django.utils.text import Truncator, slugify
 
+
 User = getattr(settings, "AUTH_USER_MODEL")
+
 
 class StatusChoices(models.TextChoices):
     ACTIVE = "active", "Active"
@@ -12,8 +15,8 @@ class StatusChoices(models.TextChoices):
 class Post(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(blank=True, null=True, unique=True)
+    title = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(blank=True, null=True)
     content = models.TextField()
     image = models.ImageField(default="default.jpg")
     status = models.CharField(max_length=15, choices=StatusChoices.choices, default=StatusChoices.ACTIVE)
@@ -21,13 +24,12 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    def save(self, **kwargs):
+        self.slug = slugify(self.title[:20] + get_random_string(5))
+        super().save(**kwargs)
+
     def __str__(self):
         return f"{self.title} {self.user_id}"
-    
-    def save(self, **kwargs):
-        if self.slug is None:
-            self.slug = slugify(self.title)[:10]
-        super().save(**kwargs)
 
     @property
     def short_content(self):
