2c60f127fc4af022c6e4e509235e07cc43db7be1
raphael apeh <raphaelapeh37@gmail.com>
2025-03-24 21:55:47 +0100

    clean up

--- src/urls.py
+++ src/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("posts.urls")),
-    path("api/", include("posts.api.urls"))
+    path("api/", include("posts.api.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
